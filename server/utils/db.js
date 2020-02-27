682e3700d3314276f81d756b393501b93dbb4644
Cesar Munoz <c-cesar.munoz@fourwindsinteractive.com>
2020-02-27 19:19:53 -0300

    Working with promises

---
+++ server/utils/db.js
@@ -0,0 +1,14 @@
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'THEHOST',
+    port: 0,
+    user: 'THEUSER',
+    password: 'THEPASSWORD',
+    database: 'THEDATABASE'
+});
+
+// connection.query = util.promisify(connection.query);
+
+module.exports = connection;
