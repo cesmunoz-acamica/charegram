b59fb52a134a724af529bd6db9cc7679b3fc1c59
Cesar Munoz <c-cesar.munoz@fourwindsinteractive.com>
2020-03-03 21:22:04 -0300

    Adding dotenv

--- server/utils/db.js
+++ server/utils/db.js
@@ -2,11 +2,11 @@ const util = require('util');
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'THEHOST',
-    port: 0,
-    user: 'THEUSER',
-    password: 'THEPASSWORD',
-    database: 'THEDATABASE'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 connection.query = util.promisify(connection.query);
